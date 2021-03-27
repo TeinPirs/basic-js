657b3b2cb9e006490a481d502f9e1bac0eec7790
Kalinichenko Anatoliy <tein.pirs@mail.ru>
2021-03-28 02:01:49 +0300

    add: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity !== "string" || !sampleActivity) return false;
+    let floatSampleActivity = parseFloat(sampleActivity);
+    if (
+        floatSampleActivity <= 0 ||
+        floatSampleActivity > 15 ||
+        isNaN(floatSampleActivity)
+    )
+        return false;
+
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / floatSampleActivity) / k);
+    return t;
 };
