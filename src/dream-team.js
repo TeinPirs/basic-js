35fad15dfee58d90f8b267646a88f9c20e0bddbc
Kalinichenko Anatoliy <tein.pirs@mail.ru>
2021-03-28 02:03:49 +0300

    add; dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (Array.isArray(members) == false) return false;
+
+    let result = [];
+
+    for (let element of members) {
+        if (typeof element == "string") {
+            result.push(element.trim()[0].toUpperCase());
+        }
+    }
+
+    return result.sort().join("");
 };
